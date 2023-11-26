67e62697ba38fe9ed93919a500ee0858eef74cc0
Zbynek Svoboda <wentura17@seznam.cz>
2023-11-26 17:48:48 +0100

    nedelni build

--- components/aDozvedetSeVic.jsx
+++ components/aDozvedetSeVic.jsx
@@ -1,9 +1,9 @@
 export default function ADozvedetSeVic({ ...props }) {
-  let title = props.title;
-  let href = props.href;
-  return (
-    <a className="text-indigo-500 inline-flex items-left" href={href}>
-      {title}
-    </a>
-  );
+	let title = props.title;
+	let href = props.href;
+	return (
+		<a className="text-indigo-500 inline-flex items-left" href={href}>
+			{title}
+		</a>
+	);
 }
