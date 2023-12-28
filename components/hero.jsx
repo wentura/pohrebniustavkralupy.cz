84c6bb55f34391855632c47f45200ab42546a1aa
Zbynek Svoboda <wentura17@seznam.cz>
2023-12-28 11:45:59 +0100

    nove logo

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,11 +1,13 @@
 import Logo from "./logo";
+import Logo2 from "./logo2";
 
 export default function Hero() {
   return (
     <section className="body-font">
       <div className="container mx-auto flex  items-center justify-center flex-col mt-[-80px] hero-background">
         <div className="px-5 pt-44 pb-32 h-full">
-          <Logo klas="max-h-60 md:max-h-96 mx-auto" />
+          {/* <Logo klas="max-h-60 md:max-h-96 mx-auto" /> */}
+          <Logo2 />
           {/* <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
             Microdosing synth tattooed vexillologist
           </h1>
