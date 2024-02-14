9614935050f18a6f33d84f726ea14127d220c1f6
Zbynek Svoboda <wentura17@seznam.cz>
2024-02-14 21:05:01 +0100

    menu anchor A

--- components/menu.jsx
+++ components/menu.jsx
@@ -9,30 +9,28 @@ export default function Menu({ klas }) {
       <ul className="flex px-1 gap-6 text-base">
         {menuItems.map((menuItem) => {
           return menuItem.children ? (
-            <li key={menuItem.id}>
-              <div className="menuParent">
-                <summary>
-                  {menuItem.text} <span className="text-xs">&darr;</span>{" "}
-                </summary>
-                <ul className="p-2 w-64 bg-modra menuKid absolute">
-                  {/* {menuItem.children.length} */}
-                  {menuItem.children.map((menuChildren) => {
-                    return (
-                      <li key={menuChildren.id} className="p-3">
-                        <Link
-                          rel=""
-                          href={menuChildren.link}
-                          className="odkaz-bila"
-                          // scroll={menuChildren.scroll}
-                          scroll={true}
-                        >
-                          {menuChildren.text}
-                        </Link>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
+            <li key={menuItem.id} className="menuParent">
+              <summary>
+                {menuItem.text} <span className="text-xs">&darr;</span>{" "}
+              </summary>
+              <ul className="p-2 w-64 bg-modra menuKid absolute">
+                {/* {menuItem.children.length} */}
+                {menuItem.children.map((menuChildren) => {
+                  return (
+                    <li key={menuChildren.id} className="p-3">
+                      <a
+                        rel=""
+                        href={menuChildren.link}
+                        className="odkaz-bila"
+                        // scroll={menuChildren.scroll}
+                        // scroll={true}
+                      >
+                        {menuChildren.text}
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
             </li>
           ) : (
             <li key={menuItem.id}>
