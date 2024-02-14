568e2bbb52bc1de43477cc59ccf141197d01b873
Zbynek Svoboda <wentura17@seznam.cz>
2024-02-14 21:12:46 +0100

    menu unique key :)

--- components/menu.jsx
+++ components/menu.jsx
@@ -4,20 +4,25 @@ import { menuItems } from "./data-menu";
 
 export default function Menu({ klas }) {
   klas ? klas : "";
+  let i;
   return (
     <div>
       <ul className="flex px-1 gap-6 text-base">
         {menuItems.map((menuItem) => {
+          i + 1;
           return menuItem.children ? (
-            <li key={menuItem.id} className="menuParent">
+            // <li key={menuItem.id} className="menuParent">
+            <li key={i} className="menuParent">
               <summary>
                 {menuItem.text} <span className="text-xs">&darr;</span>{" "}
               </summary>
               <ul className="p-2 w-64 bg-modra menuKid absolute">
                 {/* {menuItem.children.length} */}
                 {menuItem.children.map((menuChildren) => {
+                  i + 1;
                   return (
-                    <li key={menuChildren.id} className="p-3">
+                    // <li key={menuChildren.id} className="p-3">
+                    <li key={i} className="p-3">
                       <a
                         rel=""
                         href={menuChildren.link}
