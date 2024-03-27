d2a05af8fb8d9a7a5b6d6fb61b23b0c467957db0
Zbynek Svoboda <wentura17@seznam.cz>
2024-03-27 19:41:21 +0100

    favicon

--- app/(content)/o-nas/historie/page.jsx
+++ app/(content)/o-nas/historie/page.jsx
@@ -156,7 +156,7 @@ export default function Onas() {
       </section>
       <section className="text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-16 md:flex-row flex-col items-center">
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+          <div className="max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
             <img
               className="object-cover object-center rounded max-w-sm mx-auto"
               alt="hero"
