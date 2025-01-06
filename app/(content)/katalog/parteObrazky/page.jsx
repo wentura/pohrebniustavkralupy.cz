d996ceacb0e8b2fc4f967fbc7550c9a5dff43fe2
Zbynek Svoboda <wentura17@seznam.cz>
2025-01-06 11:20:07 +0100

    Image ->>> back to img

--- app/(content)/katalog/parteObrazky/page.jsx
+++ app/(content)/katalog/parteObrazky/page.jsx
@@ -147,7 +147,7 @@ export default function NaSmutecniOznameni() {
               return (
                 <div className="xl:w-1/4 md:w-1/3 px-4 py-2" key={index}>
                   <div className="mb-8">
-                    <Image
+                    <img
                       className="rounded w-full"
                       src={item.href}
                       alt="ukázkové smuteční oznámení (parte), Pohřební ústav Kralupy nad Vltavou"
@@ -174,7 +174,7 @@ export default function NaSmutecniOznameni() {
               return (
                 <div className="xl:w-1/4 md:w-1/3 px-4 py-2" key={index}>
                   <div className="mb-8">
-                    <Image
+                    <img
                       className="rounded w-full"
                       src={item.href}
                       alt="ukázkové smuteční oznámení (parte), Pohřební ústav Kralupy nad Vltavou"
@@ -201,7 +201,7 @@ export default function NaSmutecniOznameni() {
               return (
                 <div className="xl:w-1/4 md:w-1/3 px-4 py-2" key={index}>
                   <div className="mb-8">
-                    <Image
+                    <img
                       className="rounded w-full"
                       src={item.href}
                       alt="ukázkové smuteční oznámení (parte), Pohřební ústav Kralupy nad Vltavou"
